8aebfd782e3f3f4e7c413fc37973ba5b97dd36f7
Manu-rca-git <Manuelramoncarrasco05@gmail.com>
2024-11-06 18:28:04 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// Arrays con las palabras
+let pronounList = ['my', 'your', 'our', 'their'];
+let adjectiveList = ['awesome', 'creative', 'unique', 'fantastic'];
+let nounList = ['developer', 'artist', 'thinker', 'explorer'];
+let extensionList = ['.com', '.net', '.org', '.io'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// Generar nombres de dominio básicos
+for (let pronounIndex = 0; pronounIndex < pronounList.length; pronounIndex++) {
+    for (let adjectiveIndex = 0; adjectiveIndex < adjectiveList.length; adjectiveIndex++) {
+        for (let nounIndex = 0; nounIndex < nounList.length; nounIndex++) {
+            // Genera combinaciones con cada extensión
+            for (let extensionIndex = 0; extensionIndex < extensionList.length; extensionIndex++) {
+                console.log(pronounList[pronounIndex] + adjectiveList[adjectiveIndex] + nounList[nounIndex] + extensionList[extensionIndex]);
+            }
+        }
+    }
+}
